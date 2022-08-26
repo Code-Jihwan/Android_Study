d9da0b46c263378deb1aed58fc6e7f9e85398971
jihwan <jihwan3194@naver.com>
2022-08-26 21:29:21 +0900

    fragment_bottomnavigationview 수정

--- fragment_bottomnavigationview/src/main/java/com/example/fragment_bottomnavigationview/MainActivity.java
+++ fragment_bottomnavigationview/src/main/java/com/example/fragment_bottomnavigationview/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.fragment_bottomnavigationview;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -10,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -43,13 +43,14 @@ public class MainActivity extends AppCompatActivity {
                         Log.wtf("MainActivity", "menu1 Click");       //Log 보기
                         fragmentManager.beginTransaction().replace(R.id.layout00, fragment1).commit();
                         transaction.addToBackStack(null);   //.addToBackStack(null) 추가 시 -> 스택에 쌓이므로 뒤로가기 시 스택에 있는 프래그먼트들이 순서대로 onDestory 된다.
-                                                            // 뒤로 가기 기능.
-                        break;
+                        break;                              // 뒤로 가기 기능.
+
                     case R.id.menu2:
                         Log.wtf("MainActivity", "menu2 Click");
                         fragmentManager.beginTransaction().replace(R.id.layout00, fragment2).commit();
                         transaction.addToBackStack(null);
                         break;
+
                     case R.id.menu3:
                         Log.wtf("MainActivity", "menu3 Click");
                         fragmentManager.beginTransaction().replace(R.id.layout00, fragment3).commit();
