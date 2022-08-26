d75780c45a548ce49f32cbeb87710c08faba9e31
jihwan <jihwan3194@naver.com>
2022-08-26 20:29:57 +0900

    code 추가 및 수정

--- fragment_bottomnavigationview/src/main/java/com/example/fragment_bottomnavigationview/MainActivity.java
+++ fragment_bottomnavigationview/src/main/java/com/example/fragment_bottomnavigationview/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.fragment_bottomnavigationview;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -17,36 +20,45 @@ public class MainActivity extends AppCompatActivity {
     private Fragment2 fragment2 = new Fragment2();
     private Fragment3 fragment3 = new Fragment3();
 
+    private BottomNavigationView bottomNavigationView;
+    private LinearLayout containerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        containerView = findViewById(R.id.layout00);
+
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.layout00, fragment1, null);
+        transaction.commit();
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
-            @SuppressLint("NonConstantResourceId")
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onNavigationItemReselected(@NonNull MenuItem item) {
-
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.menu1:
-                        transaction.replace(R.id.layout00, fragment1).commitAllowingStateLoss();
+                        Log.wtf("MainActivity", "menu1 Click");       //Log 보기
+                        fragmentManager.beginTransaction().replace(R.id.layout00, fragment1).commit();
                         transaction.addToBackStack(null);   //.addToBackStack(null) 추가 시 -> 스택에 쌓이므로 뒤로가기 시 스택에 있는 프래그먼트들이 순서대로 onDestory 된다.
+                                                            // 뒤로 가기 기능.
                         break;
                     case R.id.menu2:
-                        transaction.replace(R.id.layout00, fragment2).commitAllowingStateLoss();
+                        Log.wtf("MainActivity", "menu2 Click");
+                        fragmentManager.beginTransaction().replace(R.id.layout00, fragment2).commit();
                         transaction.addToBackStack(null);
                         break;
                     case R.id.menu3:
-                        transaction.replace(R.id.layout00, fragment3).commitAllowingStateLoss();
+                        Log.wtf("MainActivity", "menu3 Click");
+                        fragmentManager.beginTransaction().replace(R.id.layout00, fragment3).commit();
                         transaction.addToBackStack(null);
                         break;
                 }
+                return true;
             }
         });
     }
 }
+
